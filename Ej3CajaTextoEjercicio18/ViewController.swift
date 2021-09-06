d8c2c1d823e52b48fc9425f1fc7b16df33e80caf
MIGUEL BEASCOA <77923795+mbeascoa@users.noreply.github.com>
2021-09-06 10:55:17 +0200

    6 de septiembre

--- Ej3CajaTextoEjercicio18/ViewController.swift
+++ Ej3CajaTextoEjercicio18/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelcaja: UITextField!
+    @IBOutlet weak var mensajesalida: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func mostrarMensaje(_ sender: Any) {
+        mensajesalida.text="Hola \(labelcaja.text!)"
+    }
+    
 }
 
